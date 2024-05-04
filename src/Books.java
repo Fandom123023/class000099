efc65f02596cc3037d095a6b118544718bf3cd48
Tural <musaev.tural.94@mail.ru>
2024-05-04 23:12:34 +0300

    class0009

---
+++ src/Books.java
@@ -0,0 +1,26 @@
+public class Books {
+    private String title;
+    private Author author;
+    private int publicationYear;
+
+    public Books(String title, Author author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public int getPublicationYear() {
+        return this.publicationYear;
+    }
+
+    public void setPublicationYear(int i) {
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args){
+        Author author1 = new Author("Лев", "Толстой");
+        Author author2 = new Author("Николай", "Некрасов");
+
+        Books book1 = new Books("Хаджи Мурад", author1, 1912);
+        Books book2 = new Books("На Волге", author2, 1860);
+
+        System.out.println("Информация о книге 1:");
+        System.out.println("Название: " + book1.getTitle());
+        System.out.println("Автор: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName());
+        System.out.println("Год публикации: " + book1.getPublicationYear());
+
+        System.out.println("\nИнформация о книге 2:");
+        System.out.println("Название: " + book2.getTitle());
+        System.out.println("Автор: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName());
+        System.out.println("Год публикации: " + book2.getPublicationYear());
+
+        book1.setPublicationYear(1996);
+        System.out.println("\nОбновлённый год публикации первой книги \"" + book1.getTitle() + "\": " + book1.getPublicationYear());
+
+
+
 
     }
 
